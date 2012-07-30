cc55802e54cb7425cbf15045a588c2257eb112d1
Tim Reed <tim@alloftheabove.co.uk>
2012-07-31 00:27:06 +0100

    Styling amends

--- collection/plus.php
+++ collection/plus.php
@@ -17,7 +17,7 @@ $category = "collection";
 
 		<div id="details">
 			
-			<div style="float:left">
+			<div>
   	  			<h1 class="title"><strong>plus</strong> pepper grinder</h1>
 		  	 	<span class="description">carbonised bamboo, acrylic, rubber        
 					<span class="dimensions">h70   w55   d70</span>
@@ -30,7 +30,7 @@ $category = "collection";
 	
 
 	
-      <div id="slideshow">
+      <div id="slideshow" class="images">
       
          <!-- We will have a collection of images -->
          <div class="active"><img src="/images/lights.png" alt="" /></div>
